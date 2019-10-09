defac5ba7e5d8569bd4550247cdcd2b8a83ffb85
Zeus3hd <dafuq.ahd@gmail.com>
2019-10-09 18:01:43 +0300

    add react-router-dom and create the navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [lang, setLang] = useState(true);
+
+  const switchLang = () => {
+    setLang(!lang);
+  };
   return (
-    <div className="App">
-      <Header language={lang} />
-      <button onClick={() => setLang(!lang)}>Click</button>
-    </div>
+    <Router className="App">
+      <Navbar language={lang} switchLang={switchLang} />
+
+      <Switch>
+        <Route path="/">
+          <Header language={lang} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
